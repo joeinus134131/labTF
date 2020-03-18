90847eca7eb2744cfcb67a0fd4c22bbfc953eb02
joeinus134131 <49284230+joeinus134131@users.noreply.github.com>
2020-03-19 02:50:10 +0700

    Add files via upload

---
+++ kalkulator - Copy.ino
@@ -0,0 +1,47 @@
+
+
+void setup()
+{
+	Serial.begin(9600);
+	Serial.print("hello world");
+}
+
+void loop()
+{
+	if (Serial.available()) {
+		int inByte = Serial.read();
+		switch (inByte) {
+		case 0 :
+			Serial.println("0");
+			break;
+		case 1 :
+			Serial.println("1");
+			break;
+		case 2 :
+			Serial.println("2");
+			break;
+		case 3 :
+			Serial.println("3");
+			break;
+		case 4 :
+			Serial.println("4");
+			break;
+		case 5 :
+			Serial.println("5");
+			break;
+		case 6 :
+			Serial.println("6");
+			break;
+		case 7 :
+			Serial.println("7");
+			break;
+		case 8 :
+			Serial.println("8");
+			break;
+		case 9 :
+			Serial.println("9");
+			break;
+		default:
+	}
+}
+}
